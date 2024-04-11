ae620be42aa2b0584e28408f099f4537cbca8196
Emmy <emilyileeva@gmail.com>
2024-04-11 09:16:52 -0700

    set up about page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+puppies = [
+    {'name': 'Buddy', 'breed': 'Golden Retriever'},
+    {'name': 'Daisy', 'breed': 'Poodle'},
+    {'name': 'Rex', 'breed': 'German Shepherd'}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def puppies_index(request):
+    return render(request, 'puppies/index.html', {
+        'puppies': puppies
+    })
